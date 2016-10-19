19c8fe8c44ef160c44a630bd21e321237f5e4ddd
Random Monkey <diftrujillo1@gmail.com>
2016-10-19 13:20:26 -0500

    commit

--- R/Convert-Da_funs.R
+++ R/Convert-Da_funs.R
@@ -1,27 +1,82 @@
 library(lubridate)
 library(zoo)
 
-conv_Da2Ca_weekdays <- function(df){
+#' gg_conv_Da2Ca_weekdays. : title.
+#' Convert Data type to Category weekdays
+#' @name gg_conv_Da2Ca_weekdays.
+#' @param x A number.
+#' @param y A number.
+#' @export
+#' @return The sum of \code{x} and \code{y}.
+#' @section ftypes: Da-Nu
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+gg_conv_Da2Ca_weekdays. <- function(df){
   weekdays <- weekdays(df$a)
   return(weekdays)
 }
 
-conv_Da2Ca_months <- function(df){
+#' gg_conv_Da2Ca_months. : title.
+#' Convert Data type to Category months
+#' @name gg_conv_Da2Ca_months.
+#' @param x A number.
+#' @param y A number.
+#' @export
+#' @return The sum of \code{x} and \code{y}.
+#' @section ftypes: Da-Nu
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+gg_conv_Da2Ca_months. <- function(df){
   months <- months(df$a)
   return(months)
 }
 
-conv_Da2Ca_years <- function(df){
+#' gg_conv_Da2Ca_years. : title.
+#' Convert Data type to Category years
+#' @name gg_conv_Da2Ca_years.
+#' @param x A number.
+#' @param y A number.
+#' @export
+#' @return The sum of \code{x} and \code{y}.
+#' @section ftypes: Da-Nu
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+gg_conv_Da2Ca_years. <- function(df){
   years <- year(df$a)
   return(years)
 }
 
-conv_Da2Ca_qtr <- function(df){
+#' gg_conv_Da2Ca_qtr. : title.
+#' Convert Data type to Category quaters
+#' @name gg_conv_Da2Ca_qtr.
+#' @param x A number.
+#' @param y A number.
+#' @export
+#' @return The sum of \code{x} and \code{y}.
+#' @section ftypes: Da-Nu
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+gg_conv_Da2Ca_qtr. <- function(df){
   year_qtr <- as.yearqtr(df$a)
   return(year_qtr)
 }
 
-conv_Da2Ca_weeks <- function(df){
+#' gg_conv_Da2Ca_weeks. : title.
+#' Convert Data type to Category weeks
+#' @name gg_conv_Da2Ca_weeks.
+#' @param x A number.
+#' @param y A number.
+#' @export
+#' @return The sum of \code{x} and \code{y}.
+#' @section ftypes: Da-Nu
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+gg_conv_Da2Ca_weeks. <- function(df){
   weeks <- as.numeric(format(df$a+3, "%U"))
   return(weeks)
 }
