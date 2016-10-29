f72818b0c5594819f5374a293f8084bf97975fe1
jpmarindiaz <jpmarindiaz@gmail.com>
2016-10-29 15:52:34 -0500

    added theme_ds

---
+++ R/themes.R
@@ -0,0 +1,173 @@
+
+#' @export
+getPalette <- function(type = "qualitative", rev = FALSE){
+  dsGreen <- "#95C11E"
+  dsYellow <- "#FFED00"
+  dsMagenta <- "#E5007D"
+  dsBlue <- "#009EE3"
+  dsOrange <- "#FFA500"
+  dsPalette <- c(dsMagenta,dsBlue,dsGreen,dsOrange,dsYellow)
+  p <- dsPalette
+  if(type == "sequential") {
+    p <-  c(dsMagenta,dsBlue)
+  }
+  if(rev) p <- rev(p)
+  p
+}
+
+
+#' @export
+theme_ds <- function(){
+  type <- 'outer'
+  inner <- type == 'inner'
+  palette <- list(
+    background = "#FDF8FD", # #ffffff #F0EDFF #F8EDFA #FDF8FD
+    text = list(inner = "#555555", outer = "#111111"),
+    line = list(inner = "#826A50", outer = "#362C21"),
+    gridline = "#c9c7d3",
+    swatch = c("#111111","#65ADC2","#233B43","#E84646","#C29365","#362C21","#316675","#168E7F","#109B37"),
+    gradient = list(low = "#65ADC2", high = "#362C21")
+  )
+  spacing <- 1.6
+  line_colour <- "#1d1d1d"
+  text_colour <- "#555555"
+  text_size <- 12
+  line_weight <- 0.5
+  x_title_spacing <- function(spacing)
+    max(-1.2, -(spacing / 1.25) + 0.5)
+  y_title_spacing <- function(spacing)
+    max(0.8, min(2.4, spacing))
+
+  theme(
+    line = element_line(
+      colour = line_colour,
+      size = line_weight,
+      linetype = 1,
+      lineend = "butt"),
+    rect = element_rect(
+      fill = "white",
+      colour = text_colour,
+      size = 0.5,
+      linetype = 1),
+    text = element_text(
+      debug=FALSE,
+      margin=margin(),
+      family = '',
+      face = "plain",
+      colour = text_colour,
+      size = text_size,
+      hjust = 0.5,
+      vjust = 0.5,
+      angle = 0,
+      lineheight = 0.9),
+    axis.text = element_text(
+      debug=FALSE,
+      margin=margin(),
+      size = rel(0.8),
+      colour = text_colour),
+    strip.text = element_text(
+      debug=FALSE,
+      margin=margin(),
+      size = rel(0.8)),
+    axis.line = element_line(
+      colour = line_colour),
+    # axis.line.x = function(...) element_line(...),
+    axis.line.x = element_line(colour = line_colour),
+    # axis.line.y = function(...) element_line(...),
+    axis.line.y = element_line(colour = line_colour),
+    axis.text.x = element_text(
+      debug=FALSE,
+      margin=margin(0.1 * spacing, 0.1 * spacing, 0.1 * spacing, 0.1 * spacing, unit = 'cm'),
+      vjust = 1,
+      colour = text_colour,
+      face='bold'),
+    axis.text.y = element_text(
+      debug=FALSE,
+      margin=margin(0.1 * spacing, 0.1 * spacing, 0.1 * spacing, 0.1 * spacing, unit = 'cm'),
+      hjust = 1,
+      colour = text_colour,
+      face='bold'),
+    # axis.ticks = function(...) element_blank(),
+    axis.ticks = element_line(colour = line_colour),
+    # axis.title = function(...) element_text(face='bold', ...),
+    axis.title = element_text(face='bold',colour = text_colour),
+    axis.title.x = element_text(
+      debug=FALSE,
+      margin=margin(),
+      vjust=x_title_spacing(spacing)),
+    axis.title.y = element_text(
+      debug=FALSE,
+      margin=margin(),
+      angle = 90,
+      vjust=y_title_spacing(spacing)),
+    axis.ticks.length = grid::unit(0.15, "cm"),
+    legend.background = element_rect(
+      colour = ifelse(inner, 'white', palette$background),
+      fill = ifelse(inner, 'white', palette$background)),
+    legend.margin = grid::unit(0.2 * spacing, "cm"),
+    legend.key = element_rect(
+      colour = ifelse(inner, 'white', palette$background),
+      fill = palette$background),
+    legend.key.size = grid::unit(
+      1.2, "lines"),
+    legend.key.height = NULL,
+    legend.key.width = NULL,
+    legend.text = element_text(
+      debug=FALSE,
+      margin=margin(),
+      size = rel(0.8)),
+    legend.text.align = NULL,
+    legend.title = element_text(
+      debug=FALSE,
+      margin=margin(),
+      size = rel(0.8),
+      face = "bold",
+      hjust = 0),
+    legend.title.align = NULL,
+    legend.position = "right",
+    legend.direction = NULL,
+    legend.justification = "center",
+    legend.box = NULL,
+    panel.background = element_rect(fill = palette$background,colour = NA),
+    panel.border = element_blank(),
+    # panel.grid.major = function(...) element_line(linetype='dashed', ...),
+    # panel.grid.minor = function(...) element_blank()
+    panel.grid.major = element_line(linetype='dashed',colour = palette$gridline),
+    #panel.grid.minor = element_line(colour = palette$gridline),
+    panel.grid.minor = element_blank(),
+    panel.margin = grid::unit(0.5 * spacing, 'cm'),
+    panel.margin.x = NULL,
+    panel.margin.y = NULL,
+    panel.ontop = FALSE,
+    strip.background = element_rect(
+      fill = ifelse(inner, 'white', palette$background),
+      colour = NA),
+    #strip.text = layout$strip.text(colour=text_colour),
+    strip.text = element_text(face='bold', colour=text_colour),
+    strip.text.x = element_text(
+      debug=FALSE,
+      margin=margin(),
+      size = rel(1.1),
+      face = 'bold'),
+    strip.text.y = element_text(
+      debug=FALSE,
+      margin=margin(),
+      angle = -90,
+      face = 'bold',
+      size = rel(1.1)),
+    strip.switch.pad.grid = grid::unit(0, 'cm'),
+    strip.switch.pad.wrap = grid::unit(0, 'cm'),
+    plot.background = element_rect(
+      colour = ifelse(inner, 'white', palette$background),
+      fill = ifelse(inner, 'white', palette$background)),
+    # plot.title = function(...) element_text(face='bold', ...),
+    plot.title = element_text(
+      debug=FALSE,
+      margin=margin(0, 0, 6.6, 0),
+      size = rel(1.2),
+      vjust = spacing,
+      face='bold'),
+    plot.margin = grid::unit(c(0.625, 0.625, 0.625, 0.625) * spacing, 'cm'),
+    complete = TRUE
+  )
+}
--- inst/00-db.R
+++ inst/00-db.R
@@ -6,6 +6,18 @@ install()
 
 library(ggmagic)
 
+dsGreen <- "#95C11E"
+dsYellow <- "#FFED00"
+dsMagenta <- "#E5007D"
+dsBlue <- "#009EE3"
+dsOrange <- "#FFA500"
+dsPalette <- c(dsMagenta,dsBlue,dsGreen,dsOrange,dsYellow)
+
+##
+# Pie
+gg_pie_CaNu.(sampleData("Ca-Nu")) + theme_ds() + scale_fill_manual(values = dsPalette)
+
+
 
 ggList()
 ggList("bar.*_CaNu\\.")
@@ -18,7 +30,6 @@ data <- data.frame(sample(letters[1:3],10,replace = TRUE))
 ggWhich(data)
 
 run_gg(data,"gg_waffle.")
-
 run_gg(data,"gg_bar_ver.")
 run_gg(data,"gg_bar_ver_ord.")
 
