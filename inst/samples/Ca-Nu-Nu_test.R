48a57da8015a3a01195d29bd7d8114c4a46e49cd
diftrujillo1 <diftrujillo1@gmail.com>
2016-10-31 18:55:32 -0500

    error fixed CaNuNu, getPalette

--- inst/samples/Ca-Nu-Nu_test.R
+++ inst/samples/Ca-Nu-Nu_test.R
@@ -4,8 +4,8 @@ install()
 load_all()
 
 
-data <- data.frame(a = rep(LETTERS[1:3],10),  b = rnorm(10), c = rnorm(10))
+data <- data.frame(a = sample(LETTERS[1:3], replace = TRUE, 30),  b = rnorm(10), c = rnorm(10))
 
 gg_lines_CaNuNu.(data)
-gg_point_CaNuNu.(data)
-gg_steamgraph_CaNuNu.(data)
+gg_point_CaNuNu.(data, shape_point = 8)
+gg_streamgraph_CaNuNu.(data)
