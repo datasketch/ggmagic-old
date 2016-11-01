e54a151c252d264e9131eb1bb7b4e6fc01b682e5
diftrujillo1 <diftrujillo1@gmail.com>
2016-11-01 07:57:19 -0500

    errors fixed CaYeNu

--- R/Ca-Ye-Nu.R
+++ R/Ca-Ye-Nu.R
@@ -1,33 +1,3 @@
-#' gg_bar_stk_ver_CaYeNu.: title.
-#' Barras stacked
-#' Tiene múltiples líneas
-#' @name gg_bar_stk_ver_CaYeNu.
-#' @param x A number.
-#' @param y A number.
-#' @export
-#' @return The sum of \code{x} and \code{y}.
-#' @section ftypes: Ca-Ye-Nu,Ca-Ca-Nu
-#' @examples
-#' add(1, 1)
-#' add(10, 1)
-gg_bar_stk_ver_CaYeNu. <- function(data, title = NULL,xlab = NULL, ylab = NULL, clab = NULL, ...){
-  f <- fringe(data)
-  nms <- getCnames(f)
-  data <- f$d
-  xlab <- xlab %||% nms[2]
-  ylab <- ylab %||% nms[3]
-  clab <- clab %||% nms[1]
-  graph <- ggplot(data, aes(x = b, y = c, group = a, fill = a)) +
-    geom_bar(stat = "identity") +
-    ylab(ylab) +
-    xlab(xlab) +
-    scale_y_continuous(labels = comma) +
-    scale_fill_discrete(name = clab) +
-    ggtitle(title) + theme_ds() + scale_fill_manual(values = getPalette())
-
-  return(graph)
-}
-
 #' gg_bar_grp_ver_CaYeNu.: title.
 #' Barras stacked
 #' Tiene múltiples líneas
@@ -40,20 +10,39 @@ gg_bar_stk_ver_CaYeNu. <- function(data, title = NULL,xlab = NULL, ylab = NULL, 
 #' @examples
 #' add(1, 1)
 #' add(10, 1)
-gg_bar_grp_ver_CaYeNu. <- function(data, title = NULL,xlab = NULL, ylab = NULL, clab = NULL, ...){
+gg_bar_grp_ver_CaYeNu. <- function(data, title = NULL, xlab = NULL, ylab = NULL,
+                                   leg_pos = "right", ...){
   f <- fringe(data)
   nms <- getCnames(f)
   data <- f$d
   xlab <- xlab %||% nms[2]
   ylab <- ylab %||% nms[3]
-  clab <- clab %||% nms[1]
   graph <- ggplot(data, aes(x = b, y = c, group = a, fill = a)) +
     geom_bar( position = "dodge", stat="identity") +
-    ylab(ylab) +
-    xlab(xlab) +
     scale_y_continuous(labels = comma) +
-    scale_fill_discrete(name = clab) +
-    ggtitle(title) + theme_ds() + scale_fill_manual(values = getPalette())
+    ggtitle(title) + theme_ds() + scale_fill_manual(values = getPalette()) +
+    theme(legend.position = leg_pos) +
+    labs(title = title, x = xlab, y = ylab)
+
+  return(graph)
+}
+
+#' gg_bar_grp_hor_CaYeNu.: title.
+#' Barras stacked
+#' Tiene múltiples líneas
+#' @name gg_bar_grp_hor_CaYeNu.
+#' @param x A number.
+#' @param y A number.
+#' @export
+#' @return The sum of \code{x} and \code{y}.
+#' @section ftypes: Ca-Ye-Nu,Ca-Ca-Nu
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+gg_bar_grp_hor_CaYeNu. <- function(data, title = NULL, xlab = NULL, ylab = NULL,
+                                   leg_pos = "right", ...){
+
+  graph <- gg_bar_grp_ver_CaYeNu.(data, title, xlab, ylab, leg_pos) + coord_flip()
 
   return(graph)
 }
@@ -72,21 +61,39 @@ gg_bar_grp_ver_CaYeNu. <- function(data, title = NULL,xlab = NULL, ylab = NULL, 
 #' @examples
 #' add(1, 1)
 #' add(10, 1)
-gg_bar_stk_hor_CaYeNu. <- function(data, title = NULL,xlab = NULL, ylab = NULL, clab = NULL, ...){
+gg_bar_stk_hor_CaYeNu. <- function(data, title = NULL, xlab = NULL, ylab = NULL,
+                                   leg_pos = "right", ...){
   f <- fringe(data)
   nms <- getCnames(f)
-  xlab <- xlab %||% nms[1]
+  xlab <- xlab %||% nms[2]
   ylab <- ylab %||% nms[3]
-  clab <- clab %||% nms[2]
   data <- f$d
   graph <- ggplot(data, aes(x=a,y=c,group=factor(b),fill=factor(b))) +
     geom_bar(stat = "identity") +
-    ylab(ylab) +
-    xlab(xlab) +
-    ggtitle(title) +
     scale_y_continuous(labels = comma) +
-    scale_fill_discrete(name = clab) +
-    coord_flip() + theme_ds() + scale_fill_manual(values = getPalette())
+    coord_flip() + theme_ds() + scale_fill_manual(values = getPalette()) +
+    theme(legend.position = leg_pos) +
+    labs(title = title, x = xlab, y = ylab)
+
+  return(graph)
+}
+
+#' gg_bar_stk_ver_CaYeNu.: title.
+#' Barras stacked
+#' Tiene múltiples líneas
+#' @name gg_bar_stk_ver_CaYeNu.
+#' @param x A number.
+#' @param y A number.
+#' @export
+#' @return The sum of \code{x} and \code{y}.
+#' @section ftypes: Ca-Ye-Nu,Ca-Ca-Nu
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+gg_bar_stk_ver_CaYeNu. <- function(data, title = NULL, xlab = NULL, ylab = NULL,
+                                   leg_pos = "right", ...){
+
+  graph <- gg_bar_stk_hor_CaYeNu.(data, title, xlab, ylab, leg_pos) + coord_flip()
 
   return(graph)
 }
@@ -103,20 +110,19 @@ gg_bar_stk_hor_CaYeNu. <- function(data, title = NULL,xlab = NULL, ylab = NULL, 
 #' @examples
 #' add(1, 1)
 #' add(10, 1)
-gg_lines_hor_CaYeNu. <- function(data, title = "",xlab = NULL, ylab = NULL, clab = NULL, ...){
+gg_lines_hor_CaYeNu. <- function(data, title = "",xlab = NULL, ylab = NULL,
+                                 leg_pos = "right", ...){
   f <- fringe(data)
   nms <- getCnames(f)
   xlab <- xlab %||% nms[2]
   ylab <- ylab %||% nms[3]
-  clab <- clab %||% nms[1]
   data <- f$d
   graph <- ggplot(data, aes(x=b,y=c,group=a,colour=a)) +
     geom_line(stat = "identity") +
-    ylab(ylab) +
-    xlab(xlab) +
-    ggtitle(title) +
     scale_y_continuous(labels = comma) +
-    scale_color_manual(values = getPalette()) + theme_ds()
+    scale_color_manual(values = getPalette()) + theme_ds() +
+    theme(legend.position = leg_pos) +
+    labs(title = title, x = xlab, y = ylab)
 
   return(graph)
 }
@@ -134,22 +140,21 @@ gg_lines_hor_CaYeNu. <- function(data, title = "",xlab = NULL, ylab = NULL, clab
 #' @examples
 #' add(1, 1)
 #' add(10, 1)
-gg_circle_CaYeNu. <- function(data, title = "",xlab = NULL, ylab = NULL, clab = NULL, ...){
+gg_circle_CaYeNu. <- function(data, title = "",xlab = NULL, ylab = NULL,
+                              leg_pos = "right", ...){
 
   f <- fringe(data)
   nms <- getCnames(f)
   xlab <- xlab %||% nms[2]
   ylab <- ylab %||% nms[3]
-  clab <- clab %||% nms[1]
   data <- f$d
 
 
   graph <- ggplot(data, aes(x = b, y = a)) +
         geom_point(aes(size = c, colour = "")) +
-        ylab(ylab) +
-        xlab(xlab) +
-        ggtitle(title)+
-        theme_ds() + scale_color_manual(values = getPalette()) + guides(size = FALSE)
+        theme_ds() + scale_color_manual(values = getPalette()) + guides(size = FALSE) +
+    theme(legend.position = leg_pos) +
+    labs(title = title, x = xlab, y = ylab)
 
   return(graph)
 }
--- inst/samples/Ca-Ye-Nu_test.R
+++ inst/samples/Ca-Ye-Nu_test.R
@@ -9,9 +9,12 @@ install()
 data <- sampleData("Ca-Ye-Nu")
 
 gg_bar_stk_ver_CaYeNu.(data)
+gg_bar_stk_hor_CaYeNu.(data)
+
 gg_bar_grp_ver_CaYeNu.(data)
 gg_bar_stk_hor_CaYeNu.(data)
+
 gg_lines_hor_CaYeNu.(data)
 gg_circle_CaYeNu.(data)
-gg_steamgraph_CaYeNu.(data)
+gg_steam_CaYeNu.(data)
 
