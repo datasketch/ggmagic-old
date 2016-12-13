788e216ecf78d42e75f401981384dcf9e4649abf
diftrujillo1 <diftrujillo1@gmail.com>
2016-12-13 08:45:06 -0500

    error fixed in doc, new ftypes, new names

--- R/Ye-Nu.R
+++ R/Ye-Nu.R
@@ -1,12 +1,12 @@
 #' gg_lines_hor_YeNu.: title.
-#' Barras stacked
+#' Horizontal Lines
 #' Tiene múltiples líneas
 #' @name gg_lines_hor_YeNu.
 #' @param x A number.
 #' @param y A number.
 #' @export
 #' @return The sum of \code{x} and \code{y}.
-#' @section ftypes: Ca-Ye-Nu,Ca-Ca-Nu
+#' @section ftypes: Ca-Ye-Nu, Ye-Nu, Nu-Nu, Da-Nu
 #' @examples
 #' add(1, 1)
 #' add(10, 1)
@@ -26,19 +26,20 @@ gg_lines_hor_YeNu. <- function(data, title = "", subtitle = "", caption = "",
 }
 
 #' gg_lollipop_YeNu.
-#' lollipop
-#' segment and point
+#' lollipop. segment and point
+#' gg_lollipop_YeNu.
 #' @name gg_lollipop_YeNu.
 #' @param x A number.
 #' @param y A number.
 #' @export
 #' @return The sum of \code{x} and \code{y}.
-#' @section ftypes: Ca-Ye-Nu,Ca-Ca-Nu
+#' @section ftypes: Ca-Ye-Nu, Ye-Nu, Nu-Nu, Da-Nu
 #' @examples
 #' add(1, 1)
 #' add(10, 1)
 
-gg_lollipop_YeNu. <- function(data, title = "", subtitle = "", caption = "", xlab = NULL, ylab = NULL, size = 7,...){
+gg_lollipop_YeNu. <- function(data, title = "", subtitle = "", caption = "",
+                              xlab = NULL, ylab = NULL, size = 7,...){
 
   f <- fringe(data)
   nms <- getCnames(f)
@@ -46,11 +47,14 @@ gg_lollipop_YeNu. <- function(data, title = "", subtitle = "", caption = "", xla
   ylab <- ylab %||% nms[2]
   data <- f$d
 
-  ggplot(data, aes(x = a, y = b)) +
-    geom_segment(aes(xend=a, yend=0)) + geom_point(colour = "#009EE3", size = size) +
+  graph <- ggplot(data, aes(x = a, y = b)) +
+    geom_segment(aes(xend=a, yend=0)) + geom_point(aes(color = ""), size = size) +
     theme_ds() +
     scale_x_continuous(limits=c(min(data$a),max(data$a))) +
-    labs(title = title, subtitle = subtitle, caption = caption, x = xlab, y = ylab)
+    labs(title = title, subtitle = subtitle, caption = caption, x = xlab, y = ylab) +
+    scale_color_manual(values = getPalette())
+
+  return(graph)
 }
 
 #' gg_waterfall_YeNu.
@@ -60,7 +64,7 @@ gg_lollipop_YeNu. <- function(data, title = "", subtitle = "", caption = "", xla
 #' @param y A number.
 #' @export
 #' @return The sum of \code{x} and \code{y}.
-#' @section ftypes: Ca,Ca-Nu
+#' @section ftypes: Ca-Ye-Nu, Ye-Nu, Nu-Nu, Da-Nu
 #' @examples
 #' add(1, 1)
 #' add(10, 1)
@@ -74,22 +78,22 @@ gg_waterfall_YeNu. <- function(data, title = "", subtitle = "", caption = "", xL
   data <- f$d
 
   graph <- ggplot_waterfall(data,'a','b') + theme_ds() + theme(legend.position="none") +
-           scale_color_manual(breaks = c("+",  "-", ""), values = c("#E5007D", "#009EE3", "black")) +
+           scale_color_manual(breaks = c("+",  "-", ""), values = getPalette()) +
            scale_x_continuous(limits=c(min(data$a),max(data$a))) +
            labs(title = title, subtitle = subtitle, caption = caption, x = xlab, y = ylab)
 
-     return(graph)
+  return(graph)
 }
 
 
 #' gg_bar_coloured_x_ver_YeNu.
-#' vertical bar
+#' vertical coloured bar
 #' @name gg_bar_coloured_x_ver_YeNu.
 #' @param x A category.
 #' @param y A number.
 #' @export
 #' @return The sum of \code{x} and \code{y}.
-#' @section ftypes: Ca,Ca-Nu, Ye-Nu
+#' @section ftypes: Ca-Ye-Nu, Ye-Nu, Nu-Nu, Da-Nu
 #' @examples
 #' add(1, 1)
 #' add(10, 1)
@@ -136,7 +140,7 @@ gg_bar_coloured_x_ver_YeNu.<- function(data, titleLabel = "", subtitle = "", cap
 #' @param y A number.
 #' @export
 #' @return The sum of \code{x} and \code{y}.
-#' @section ftypes: Da-Nu
+#' @section ftypes: Ca-Ye-Nu, Ye-Nu, Nu-Nu, Da-Nu
 #' @examples
 #' add(1, 1)
 #' add(10, 1)
@@ -149,9 +153,11 @@ gg_area_YeNu. <- function(data, title = "", subtitle = "", caption = "", xlab = 
   data <- f$d
 
   graph <- ggplot(data, aes(x = a, y = b, group=1)) +
-           geom_area(fill = "#009EE3") + theme_ds() +
+           geom_area(aes(fill = ""), show.legend = FALSE) +
+    scale_fill_manual(values = getPalette()) + theme_ds() +
            scale_x_continuous(limits = c(min(data$a), max(data$a))) +
            labs(title = title, subtitle = subtitle, caption = caption, x = xlab, y = ylab)
+
   return(graph)
 }
 
--- inst/samples/Ye-Nu_test.R
+++ inst/samples/Ye-Nu_test.R
@@ -10,10 +10,10 @@ data <- data.frame(año = 2001:2016, datos = rnorm(16))
 
 gg_lines_hor_YeNu.(data)
 
-gg_lollipop_YeNu.(data)
+gg_lollipop_YeNu.(data, size = 2)
 
 gg_waterfall_YeNu.(data)
 
-gg_bar_coloured_x_ver_YeNu.(data, text = FALSE)
+gg_bar_coloured_x_ver_YeNu.(data)
 
 gg_area_YeNu.(data)
