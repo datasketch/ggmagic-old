3e91ea971bbead1d2bfe6b04bf753aa8081dc115
random monkey <camilaachury.4@gmail.com>
2017-01-05 18:02:33 -0500

    added stacked bar

--- R/Ca-Ca-Ca-Nu.R
+++ R/Ca-Ca-Ca-Nu.R
@@ -100,3 +100,53 @@ gg_treemap_z_CaCaCaNu. <- function(data, titleLabel = "", subtitle = "", caption
 
   graph
 }
+
+
+#' gg_bar_stacked_100_hor_CaCaCaNu.
+#' Stacked
+#' @name gg_bar_stacked_100_hor_CaCaCaNu.
+#' @param x A number.
+#' @param y A number.
+#' @export
+#' @return The sum of \code{x} and \code{y}.
+#' @section ftypes: Ca-Ca-Ca-Nu, Ca-Ca-Ye-Nu
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+
+
+gg_bar_stacked_100_hor_CaCaCaNu. <- function(data, titleLabel = "", subtitle = "", caption = "",
+                                             fillLabel = NULL, xlab = NULL, ylab = NULL,leg_pos = 'right', ...){
+
+  f <- fringe(data)
+  nms <- getClabels(f)
+  flabel <- fillLabel %||% nms[1]
+  data <- f$d
+
+  data$d <- abs(data$d)
+
+  data_graph <- data %>%
+    #tidyr::drop_na(a,c) %>%
+    dplyr::group_by(a, b, c) %>%
+    dplyr::summarise(count = sum(d, na.rm = TRUE)) %>%
+    tidyr::spread(c, count) %>%
+    tidyr::gather(c,count,c(-a,-b) )
+
+  data_graph[is.na(data_graph)] <- 0
+
+
+  graph <- ggplot(data_graph,aes(x = c, y = count,fill = a)) +
+    geom_bar(position = "fill",stat = "identity") +
+    coord_flip() +
+    scale_y_continuous(labels = percent_format()) +
+    theme_ds() +
+    facet_grid(b ~ .) +
+    #facet_grid(b ~ ., switch = 'y') +
+    scale_fill_manual(values=getPalette())
+
+  graph <-  graph +
+    labs(title = titleLabel, subtitle = subtitle, caption = caption, x = xlab, y = ylab) +
+    theme(legend.position = leg_pos)
+
+  graph
+}
