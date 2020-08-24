3b2a707fb649b93757d1c756dfa1bbdb69d0a64b
Camila Achury <camilaachury.4@gmail.com>
2020-08-24 09:31:41 -0300

    bar yea num

--- R/gg_bar_CatYeaNum.R
+++ R/gg_bar_CatYeaNum.R
@@ -1,32 +1,33 @@
-#' Bar Chart Cat Cat Num
+#' Bar Chart Categories Yea Numeric
 #'
 #' This chart does not allow for chaning orientation
 #'
 #' @param data A data.frame
+#' @param orientation Doesn't do anything for this type of chart.
+#' @param order doesn't do anything
 #' @inherit dsvizopts::dsviz_default_opts
 #' @inheritDotParams dsvizopts::dsviz_default_opts
 #' @section ctypes:
-#' Cat-Cat-Num
+#' Cat-Yea-Num, Cat-Yea
 #' @examples
-#' gg_bar_CatNum(sample_data("Cat-Num", nrow = 10))
+#' gg_bar_CatYeaNum(sample_data("Cat-Yea-Num", nrow = 10))
 #' @export
-gg_bar_CatCatNum <- function(data, ...){
+gg_bar_CatYeaNum <- function(data, ...){
 
   if (is.null(data)) stop("need dataset to visualize")
   opts <- dsvizopts::merge_dsviz_options(...)
 
-  l <- ggmagic_prep(data, opts)
-
-  d <- l$d
+  l <- ggmagic_prep(data, opts, family = "bar")
+  l$d$b <- as.character(l$d$b)
 
   ggpos <- "stack"
   if(l$extra$graph_type == "grouped")
     ggpos <- position_dodge2(width = 0.9, preserve = "single")
 
-  #gg <- ggplot(l$d, aes(x = a, y = c, fill = ..colors ))
-  gg <- ggplot(l$d, aes(x = a, y = c, fill = b )) +
+
+  gg <- ggplot(l$d, aes(x = b, y = c, fill = a)) +
     geom_bar(stat = "identity", position = ggpos) +
-    scale_fill_identity() +
+    scale_fill_identity()  +
     labs(title = l$titles$title,
          subtitle = l$titles$subtitle,
          caption = l$titles$caption,
@@ -40,6 +41,5 @@ gg_bar_CatCatNum <- function(data, ...){
 
   gg <- gg + add_ggmagic_theme(opts$theme)
   add_branding_bar(gg, opts$theme)
-
+ gg
 }
-
