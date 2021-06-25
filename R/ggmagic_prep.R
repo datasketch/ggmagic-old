ac66c9e39b83f3c095ca82c1ada12ae97b591d84
Camila Achury <camilaachury.4@gmail.com>
2021-06-25 10:34:28 -0300

    add data prep from dsvizprep

--- R/ggmagic_prep.R
+++ R/ggmagic_prep.R
@@ -5,191 +5,83 @@
 # bar_CatNum f_cats f_nums
 # dataLabels_location extra for pie
 
-ggmagic_prep <- function(data, opts = NULL,
-                         extra_pattern = ".", family = ""){
+ggmagic_prep <- function(data, opts = NULL, extra_pattern = ".", plot =  "bar", ftype = "Cat-Num") {
 
-  # Handle case if palette_colors = NULL
+  if (is.null(data)) return()
+
+  # color -------------------------------------------------------------------
+
+  palette_type <- opts$theme$palette_type %||% "categorical"
   if(is.null(opts$theme$palette_colors)){
-    opts$theme$palette_colors <- opts$theme$palette_colors_categorical
+    opts$theme$palette_colors <- opts$theme[[paste0("palette_colors_", palette_type)]]
   }
+  palette_colors <- opts$theme$palette_colors
 
-  # Handle homodatum
-  f <- homodatum::fringe(data)
+  # data preparation by type
+  list_d <- dsvizprep::data_charts_prep(data = data,
+                                        ftype = ftype,
+                                        agg =  opts$summarize$agg,
+                                        color_by = opts$style$color_by,
+                                        ptage_col = opts$postprocess$percentage_col,
+                                        drop_na = opts$preprocess$drop_na,
+                                        na_label = opts$preprocess$na_label,
+                                        drop_na_legend = opts$preprocess$drop_na_legend,
+                                        sort_opts = opts$postprocess$sort,
+                                        slice_n = opts$postprocess$slice_n,
+                                        palette = palette_colors,
+                                        highlight_value = opts$chart$highlight_value,
+                                        highlight_value_color = opts$chart$highlight_value_color,
+                                        order_legend = opts$postprocess$order_legend,
+                                        order = opts$postprocess$order,
+                                        label_wrap_legend = opts$style$label_wrap_legend,
+                                        label_wrap = opts$style$label_wrap,
+                                        group_extra_num = TRUE)
+  # format setting of data being displayed
+  data_format <- dsvizprep::format_prep(data = list_d$data,
+                                        dic = list_d$dic,
+                                        formats = list(sample_num = opts$style$format_sample_num,
+                                                       sample_cat = opts$style$format_sample_cat,
+                                                       prefix = opts$style$prefix,
+                                                       suffix = opts$style$suffix))
 
-  needs_CatNum_agg <- f$frtype %in% c("Cat", "Dat", "Yea")
-  needs_CatCat_agg <- f$frtype %in% c("Cat-Cat", "Cat-Dat" ,"Cat-Yea")
-  is_CatCat <- grepl("Cat-Cat", f$frtype)
-  label_position <- NULL
-  nms <- fringe_labels(f)
-  d <- fringe_d(f)
+  # axis labels -------------------------------------------------------------
 
-  if(needs_CatNum_agg){
-    d <- d %>%
-      dplyr::group_by_all() %>%
-      dplyr::summarise(b = n())
-    f$frtype <- paste0(f$frtype, "-Num")
-  }
+  nms_dic <- list_d$nms[names(list_d$nms) %in% c("a", "b", "c", "d")]
 
-  if(needs_CatCat_agg){
-    d <- d %>%
-      dplyr::group_by_all() %>%
-      dplyr::summarise(c = n())
-    f$frtype <- paste0(f$frtype, "-Num")
-  }
+  labelsXY <- dsvizprep::labelsXY(hor_title = opts$title$hor_title %||% nms_dic[[(1 + (length(nms_dic) - 2))]], #
+                                  ver_title = opts$title$ver_title %||% nms_dic[[length(nms_dic)]],
+                                  nms = nms_dic, orientation = opts$chart$orientation)
 
-
-  if(f$frtype %in% c("Cat-Dat-Num","Cat-Yea-Num")){
-    labelsXY <- opts$title$hor_title %||% nms[2]
-    labelsXY[2] <- opts$title$ver_title %||% nms[3]
-    color_title <- opts$title$color_title %||% nms[1]
-  }else{
-    labelsXY <- opts$title$hor_title %||% nms[1]
-    labelsXY[2] <- opts$title$ver_title %||% nms[2]
-    color_title <- NULL
-  }
-  if(opts$chart$orientation == "hor") labelsXY <- rev(labelsXY)
-  hor_title <- labelsXY[1]
-  ver_title <- labelsXY[2]
-
-  # Drop NAs
-  # TODO: Add NAs as categories or dates when it makes sense
-  d <- dsvizopts::preprocessData(d, drop_na = opts$preprocess$drop_na,
-                                 na_label = opts$preprocess$na_label, na_label_cols = "a")
-
-  # Summarize
-  if(f$frtype %in% c("Cat-Num", "Dat-Num", "Yea-Num")){
-    d <- dsvizopts::summarizeData(d, opts$summarize$agg, to_agg = b, a)
-    if (opts$postprocess$percentage) {
-      d$b <- (d$b/sum(d$b))*100
-      opts$style$suffix <- "%"
-    }
-    d <- ggmagic::labelPosition(d, "b", opts$style$label_ratio)
-
-    #if (family == "pie") d$labPos <- cumsum(d[["b"]][order(d$a, decreasing = TRUE)]) - d[["b"]][order(d$a, decreasing = TRUE)] / 2
-
-  }
-
-  if(f$frtype %in% c("Cat-Cat-Num", "Cat-Yea-Num", "Cat-Dat-Num")){
-    d <- summarizeData(d, opts$summarize$agg, to_agg = c, a, b)
-    if (opts$postprocess$percentage) {
-      by_col <- opts$postprocess$percentage_col
-
-      if (is.null(by_col)) {
-        by_col <- "a"
-      } else {
-        by_col <- names(nms[match(by_col, nms)])
-      }
-
-      d <- d %>% group_by_(by_col) %>%
-        dplyr::mutate(c = (c / sum(c, na.rm = TRUE)) * 100)
-      opts$style$suffix <- "%"
-    }
-
-    if (opts$chart$graph_type == "grouped") {
-      d <- ggmagic::labelPosition(d, "c", opts$style$label_ratio, zeroToNa = TRUE)
-      label_position <- position_dodge(width = 0.55)
-    } else {
-      d$..labpos <- d$c
-      label_position <-  position_stack(vjust = 0.5)
-      if (family %in% c("area")) label_position <-  position_stack(vjust = 1)
-    }
-  }
-
-  # Postprocess
-  if(f$frtype == c("Cat-Num")){
-    d <- dsvizopts::postprocess(d, "b", sort = opts$postprocess$sort,
-                                slice_n = opts$postprocess$slice_n)
-  }
-
-  # Complete missing with 0s
-  if(family == "area" && opts$extra$graph_type == "stacked"){
-    d <- d %>%
-      ungroup() %>%
-      #filter(!is.na(b)) %>% # Remove missing years
-      tidyr::complete(a,nesting(b), fill = list(c = 0))
-
-  }
-
-  if(f$dic$hdType[1] == "Cat"){
-    d <- dsvizopts::order_category(d, col = "a", order = opts$postprocess$order,
-                                   label_wrap = opts$style$label_wrap, new_line = "\n")
-  }
-
-
-  # Styles
-  # Handle colors
-  if(!is.null(opts$style$color_by)){
-    if(!is.numeric(opts$style$color_by)){
-      color_by <- names(nms[match(opts$style$color_by, nms)])
-      if(is.na(color_by)) stop("color_by column not found")
-    }
-  } else{
-    color_by <- opts$style$color_by
-  }
-  # color_by <- "a" pie
-
-  palette <- opts$theme$palette_colors
-
-  d$..colors <- paletero::map_colors(d, color_by, palette, colors_df = NULL)
-  if(f$frtype %in% c("Cat-Dat-Num", "Cat-Yea-Num", "Cat-Num-Num", "Cat-Num-Num-Num") && family %in% c("line","area", "bar", "treemap", "scatter")){
-    d$..colors <- paletero::map_colors(d, color_by = "a", palette, colors_df = NULL)
-  }
-  if(grepl("Cat-Cat|Cat-Yea",f$frtype)){
-    d$..colors <- paletero::map_colors(d, color_by = "b", palette, colors_df = NULL)
-  }
-
-
-
-
-  # Handle number/strings/dates formats
-  f_cats <-  makeup::makeup_format(sample = opts$style$format_cat_sample,
-                                   type = "chr" )
-  f_nums <- makeup::makeup_format(sample = opts$style$format_num_sample,
-                                  locale = opts$style$locale,
-                                  prefix = opts$style$prefix,
-                                  suffix = opts$style$suffix)
-  fmt_dataLabel <- opts$dataLabels$dataLabels_format_sample %||% opts$style$format_num_sample
-  f_nums_dataLabel <- makeup::makeup_format(sample = fmt_dataLabel,
-                                            locale = opts$style$locale,
-                                            prefix = opts$style$prefix,
-                                            suffix = opts$style$suffix)
-
-  f_dats <- makeup::makeup_format(sample = opts$style$format_dat_sample,
-                                  locale = opts$style$locale)
-
-
-  extra <- dsvizopts::get_extra_opts(opts, extra_pattern)
+  hor_title <- as.character(labelsXY[1])
+  ver_title <- as.character(labelsXY[2])
 
   list(
-    d = d,
-    colors = list(
-      c_cats = NULL # user for custom color values
-    ),
+    d = data_format,
     titles = list(
       title = opts$title$title,
       subtitle = opts$title$subtitle,
-      caption = opts$title$caption,
+      caption = opts$title$caption %||% "",
       x = hor_title,
-      y = ver_title,
-      color = color_title,
-      legend = opts$title$legend_title %||% " "
-    ),
-    orientation = opts$chart$orientation %||% "ver",
-    formats = list(
-      f_cats = f_cats,
-      f_nums = f_nums,
-      f_dats = f_dats
+      y = ver_title
     ),
     dataLabels = list(
       show = opts$dataLabels$dataLabels_show,
+      type = opts$dataLabels$dataLabels_type,
       color = opts$dataLabels$dataLabels_color %||% opts$theme$text_color,
-      size = (opts$dataLabels$dataLabels_size %||% opts$theme$text_size)/3,
-      f_nums = f_nums_dataLabel,
-      f_label_position =  label_position
+      size = (opts$dataLabels$dataLabels_size  %||% opts$theme$text_size) /3,
+      dataLabels_text_outline = opts$dataLabels$dataLabels_text_outline,
+      format_dataLabels = format_dataLabels
     ),
-    extra = extra,
-    theme = opts$theme
+    theme = c(opts$theme,
+              isNullCaption = is.null(opts$title$caption),
+              bar_pointWidth = opts$theme$bar_pointWidth,
+              credits = show_caption,
+              y_credits = y_caption,
+              suffix = suffix_enter,
+              prefix = opts$style$prefix),
+    spline = opts$style$spline,
+    graph_type = opts$chart$graph_type,
+    extra = dsvizopts::get_extra_opts(opts, extra_pattern)
   )
-
 }
 
