27f43f0b043122febd1272f2449693c30b9ced3c
Camila Achury <camilaachury.4@gmail.com>
2021-06-29 11:21:27 -0300

    add pie and donut Cat

--- R/gg_pie_CatNum.R
+++ R/gg_pie_CatNum.R
@@ -54,7 +54,36 @@ gg_pie_CatNum <- function(data = NULL,
 #' @examples
 #' gg_pie_Cat(sample_data("Cat", nrow = 10))
 #' @export gg_pie_Cat
-gg_pie_Cat <- gg_pie_CatNum
+gg_pie_Cat <- function(data = NULL,
+                       ...) {
+
+  if (is.null(data)) stop("need dataset to visualize")
+  data[[1]] <- homodatum::as_Cat(data[[1]])
+
+  opts <- dsvizopts::merge_dsviz_options(...)
+  l <- ggmagic_prep(data, opts, extra_pattern = "pie", ftype = "Cat")
+
+  gg <- ggplot(l$d, aes(x = 1, y = value, fill = a)) +
+    geom_bar(width = 1, stat = "identity", color = l$theme$background_color) +
+    scale_fill_manual(l$titles$x,values = l$d$..colors) +
+    coord_polar(theta = "y") +
+    labs(title = l$titles$title,
+         subtitle = l$titles$subtitle,
+         caption = l$titles$caption)
+  if(l$dataLabels$show){
+    gg <- gg +  geom_text(aes(#y =..ylabpos,
+      label = l$dataLabels$f_nums(value)),
+      position = position_stack(vjust = 0.5),
+      check_overlap = TRUE,
+      size = l$dataLabels$size,
+      color = l$dataLabels$color)
+  }
+
+  gg <- gg + add_ggmagic_theme_clean(opts$theme)
+  add_branding_bar(gg, opts$theme)
+
+}
+
 
 
 #' Donut (categories, numbers)
@@ -119,4 +148,40 @@ gg_donut_CatNum <- function(data = NULL,
 #' @examples
 #' gg_donut_Cat(sample_data("Cat", nrow = 10))
 #' @export gg_pie_Cat
-gg_donut_Cat <- gg_donut_CatNum
+gg_donut_Cat <- function(data = NULL,
+                         ...) {
+
+  if (is.null(data)) stop("need dataset to visualize")
+  data[[1]] <- homodatum::as_Cat(data[[1]])
+  data[[2]] <- homodatum::as_Num(data[[2]])
+
+  opts <- dsvizopts::merge_dsviz_options(...)
+  l <- ggmagic_prep(data, opts, extra_pattern = "donut", ftype = "Cat")
+
+  # Calculate width
+  #dwidth <- -1/(d$x) + 1.5
+  l$extra$donut_width <- ifelse(l$extra$donut_width < 0.01, 0.01, l$extra$donut_width)
+  x_donut_min <- -1/(l$extra$donut_width) + 1.5
+
+  d <- l$d
+  gg <- ggplot(d, aes(x = 1, y = value, fill = a)) +
+    geom_bar(width = 1, stat = "identity", color = l$theme$background_color) +
+    scale_fill_manual(l$titles$x, values = d$..colors) +
+    xlim(x_donut_min, 1.5) +
+    coord_polar(theta = "y") +
+    labs(title = l$titles$title,
+         subtitle = l$titles$subtitle,
+         caption = l$titles$caption)
+  if(l$dataLabels$show){
+    gg <- gg +  geom_text(aes(#y =..ylabpos,
+      label = l$dataLabels$f_nums(value)),
+      position = position_stack(vjust = 0.5),
+      check_overlap = TRUE,
+      size = l$dataLabels$size,
+      color = l$dataLabels$color)
+  }
+
+  gg <- gg + add_ggmagic_theme_clean(opts$theme)
+  add_branding_bar(gg, opts$theme)
+
+}
